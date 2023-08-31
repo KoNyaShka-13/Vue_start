a9bc95c875f986d616dd1408dcabdaa1bfbf988a
KoNyaShKa <ggiziawpgg@gmail.com>
2023-08-31 10:50:05 +0300

    Настройка Vue

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# Vue_start
-Изучение и параллельное создание проекта с использованием Vue 3
+# vue_start
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <div>
+            <button v-on:click="addLike">Like</button>
+            <button @click="addDislike">Dislike</button>
+        </div>
+
+        <div>Кол-во лайков: <strong>{{ likes }}</strong></div>
+        <div>Кол-во дизлайков: <strong>{{ dislikes }}</strong></div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            likes: 0,
+            dislikes: 0,
+        }
+    },
+    metods: {
+        addLike() {
+            this.likes +=1;
+        },
+        addDislike() {
+            this.dislikes +=1;
+        }
+    }
+}
+</script>
+
+
+<style>
+
+</style>
