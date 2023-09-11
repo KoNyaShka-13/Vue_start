29622feb72c938f223fccc98b2c25d4990fe3ea6
KoNyaShKa <ggiziawpgg@gmail.com>
2023-09-11 16:18:56 +0300

    V-model
    Другой способ работы с V-model

--- src/components/UI/MyInput.vue
+++ src/components/UI/MyInput.vue
@@ -1,16 +1,16 @@
 <template>
-    <input :value="modelValue" @input="updateInput" class="input" type="text">
+    <input :value="value" @input="updateInput" class="input" type="text">
   </template>
   
   <script>
   export default {
     name: 'my-input',
     props: {
-      modelValue: [String, Number]
+      value: [String, Number]
     },
     methods: {
       updateInput(event) {
-        this.$emit('update:modelValue', event.target.value)
+        this.$emit('update:value', event.target.value)
       }
     }
   }
